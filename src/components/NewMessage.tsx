442fd68f0fd12a6573ed8d0ecbb9aad123accc1a
Engine Seunghyun Park <engine@zoyi.co>
2016-12-04 16:46:01 +0900

    Modify default tab spaces

--- src/components/NewMessage.tsx
+++ src/components/NewMessage.tsx
@@ -4,61 +4,61 @@ import * as moment from "moment";
 import haversine from "../haversine";
 
 interface NewMessageProps {
-    onSubmit: (value: string) => void;
+  onSubmit: (value: string) => void;
 }
 
 interface NewMessageState {
-    value: string;
+  value: string;
 }
 
 export default class NewMessage extends React.Component<NewMessageProps, NewMessageState> {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.state = {
-            value: "",
-        };
-    }
+    this.state = {
+      value: "",
+    };
+  }
 
-    handleChange(event) {
-        this.setState({
-            value: event.target.value
-        });
-    }
+  handleChange(event) {
+    this.setState({
+      value: event.target.value
+    });
+  }
 
-    handleSubmit(event) {
-        this.props.onSubmit(this.state.value);
-        event.preventDefault();
-    }
+  handleSubmit(event) {
+    this.props.onSubmit(this.state.value);
+    event.preventDefault();
+  }
 
-    render() {
-        return (
-            <form onSubmit={this.handleSubmit.bind(this)} style={styles.form}>
-                <textarea onChange={this.handleChange.bind(this)} style={styles.input} value={this.state.value} />
-                <input type="submit" value="Comment" style={styles.submit} />
-            </form>
-        );
-    }
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit.bind(this)} style={styles.form}>
+        <textarea onChange={this.handleChange.bind(this)} style={styles.input} value={this.state.value} />
+        <input type="submit" value="Comment" style={styles.submit} />
+      </form>
+    );
+  }
 }
 
 const styles = {
-    form: {
-        alignItems: "stretch",
-        display: "flex",
-    },
+  form: {
+    alignItems: "stretch",
+    display: "flex",
+  },
 
-    input: {
-        fontSize: 16,
-        outline: "none",
-        flexGrow: 1,
-        padding: 12,
-    },
+  input: {
+    fontSize: 16,
+    outline: "none",
+    flexGrow: 1,
+    padding: 12,
+  },
 
-    submit: {
-        fontSize: 20,
-        backgroundColor: "#FF00000",
-        border: "none",
-        color: "white",
-        outline: "none",
-    },
+  submit: {
+    fontSize: 20,
+    backgroundColor: "#FF00000",
+    border: "none",
+    color: "white",
+    outline: "none",
+  },
 };
